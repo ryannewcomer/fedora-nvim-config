5a1aa3598b63c9e9204cdd004b2ea9beec982c8c
Ryan Hung <ryanhungryan@gmail.com>
2026-01-31 14:22:20 +0000

    add comment.nvim and friendly-snippet

--- init.lua
+++ init.lua
@@ -654,12 +654,12 @@ require('lazy').setup(
             -- `friendly-snippets` contains a variety of premade snippets.
             --    See the README about individual language/framework/plugin snippets:
             --    https://github.com/rafamadriz/friendly-snippets
-            -- {
-            --   'rafamadriz/friendly-snippets',
-            --   config = function()
-            --     require('luasnip.loaders.from_vscode').lazy_load()
-            --   end,
-            -- },
+            {
+              'rafamadriz/friendly-snippets',
+              config = function()
+                require('luasnip.loaders.from_vscode').lazy_load()
+              end,
+            },
           },
           opts = {},
         },
@@ -690,7 +690,7 @@ require('lazy').setup(
           -- <c-k>: Toggle signature help
           --
           -- See :h blink-cmp-config-keymap for defining your own keymap
-          preset = 'default',
+          preset = 'super-tab',
 
           -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -761,6 +761,7 @@ require('lazy').setup(
 
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+    { 'numToStr/Comment.nvim' },
 
     { -- Collection of various small independent plugins/modules
       'echasnovski/mini.nvim',
